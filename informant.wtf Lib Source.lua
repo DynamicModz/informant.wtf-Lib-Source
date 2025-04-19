0fe2b17dc8c369c18960919110eaf47717a711b0
LuaGuard <88911783+DynamicModz@users.noreply.github.com>
2025-04-19 20:47:43 +0100

    Update: Enabled watermark by default

--- informant.wtf Lib Source.lua
+++ informant.wtf Lib Source.lua
@@ -4496,7 +4496,7 @@ function library:init()
                 {'0 fps', true},
                 {'0ms', true},
             };
-            lock = 'custom';
+            lock = 'Top';
             position = newUDim2(0,0,0,0);
             refreshrate = 25;
         }
@@ -4739,8 +4739,8 @@ function library:CreateSettingsTab(menu)
     end});
 
     mainSection:AddSeparator({text = 'Watermark'})
-    mainSection:AddToggle({text = 'Enabled', flag = 'watermark_enabled'});
-    mainSection:AddList({text = 'Position', flag = 'watermark_pos', selected = 'Custom', values = {'Top', 'Top Left', 'Top Right', 'Bottom Left', 'Bottom Right', 'Custom'}, callback = function(val)
+    mainSection:AddToggle({text = 'Enabled', flag = 'watermark_enabled', state = true});
+    mainSection:AddList({text = 'Position', flag = 'watermark_pos', selected = 'Top', values = {'Top', 'Top Left', 'Top Right', 'Bottom Left', 'Bottom Right', 'Custom'}, callback = function(val)
         library.watermark.lock = val;
     end})
     mainSection:AddSlider({text = 'Custom X', flag = 'watermark_x', suffix = '%', value = 6.1, min = 0, max = 100, increment = .1});
